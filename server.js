6c88ca3229ef4d2f19026ce0f7a3b2daf7ff2c65
vanceleon <vanceleon44@gmail.com>
2020-01-29 22:18:47 -0500

    welcome world

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
 
-
 const server = express();
 server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) =>
   res.status(200).json("Welcome to the Financial Caculator Backend!")
+);
+
+server.get('/helloWorld', (req, res) =>
+  res.status(200).json("Welcome to world")
 );
 
 module.exports = server;
